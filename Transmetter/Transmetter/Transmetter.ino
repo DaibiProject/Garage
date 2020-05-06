da950f144f50d30bdc9a7a1e5fb1a4de72509677
Cydra09 <59871086+Cydra09@users.noreply.github.com>
2020-05-06 09:52:52 +0200

    Transmetter 2.0
    Maybe finish if NRF work

--- Transmetter/Transmetter/Transmetter.ino
+++ Transmetter/Transmetter/Transmetter.ino
@@ -1,102 +1,63 @@
-#include <AES.h>
-#include "./printf.h"
-#include <SPI.h> 
-#include <nRF24L01.h> 
-#include <RF24.h> 
-#define button 12
-AES aes;
-
-RF24 radio(13, 14); // CE, CSN 
-const byte addresses[][6] = {"00001", "00002"}; 
-boolean buttonState = 0; 
-
-unsigned int keyLength = 128; // Taille de la clé
-byte *key = (unsigned char*)"DbItbDotw200"; // Clé de cryptage
-byte plain[] = "Close"; // Message a encrypter 
-byte iv [N_BLOCK] ;
-unsigned long long int myIv = 36753562; // CBC initialization vector; real iv = iv x2 ex: 01234567 = 0123456701234567
-
-void setup ()
-{
-  Serial.begin (9600) ;
-  
-  pinMode(button, INPUT_PULLUP); 
-  printf_begin();
-  radio.begin(); 
-  radio.openWritingPipe(addresses[0]); // 00001 
-  radio.openReadingPipe(1, addresses[1]); // 00002 
-  radio.setPALevel(RF24_PA_MIN);
-}
+#include <AESLib.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24_config.h>
+#include <RF24.h>
  
-void loop () 
-{
-  byte iv [N_BLOCK] ;
-  int plainPaddedLength = sizeof(plain) + (N_BLOCK - ((sizeof(plain)-1) % 16)); // length of padded plaintext [B]
-  byte cipher [plainPaddedLength]; // ciphertext (encrypted plaintext)
-  byte check [plainPaddedLength]; // decrypted plaintext
-  
-  
-  
-  radio.stopListening(); 
-  buttonState = digitalRead(button); 
-  
-  while (!radio.available()){
-    if (buttonState == HIGH) {
-      Serial.println("Button on");
-      Serial.println(buttonState);
-      Serial.print("- key length : ");
-      Serial.println(keyLength);
-      aesTest (keyLength);
-      delay(2000);
-      radio.write(&buttonState, sizeof(buttonState)); 
-      radio.write(&cipher, sizeof(cipher));
-    } else if (buttonState == LOW) {
-      Serial.println("Button off");
-      radio.write(&buttonState, sizeof(buttonState));
-    }
+//nRF24L01
+#define CE_PIN 13
+#define CSN_PIN 14
+
+RF24 radio(CE_PIN,CSN_PIN);
+const byte addresses[][5] = {"03499", "02989", "51914", "44204"};
+
+void aes() {
+// AES
+byte key = (unsigned char)"DbItbDotw200";                         // Clé de cryptage
+// byte iv [N_BLOCK];
+// unsigned long long int myIv = 36753562;                          // Vecteur d'initialisation
+byte open_msg[] = "Open";                                           // Message (Open)
+byte messageCode[]= "";
+aes128_enc_single(key, open_msg);                                   // pour chiffrer
+
+for (int i = 0; i < (sizeof(open_msg)-1); i++) {
+  messageCode[i] = open_msg[i];
+ }
+Serial.println("Envoie des données");
+radio.stopListening();
+
+while (radio.available()){ 
+  for (int i = 0; i < sizeof(open_msg)-1; i++){
+    radio.write(&messageCode[i], sizeof(messageCode[i]));
+    Serial.print(messageCode[i]);                                      // Message crypter
   }
-  
 }
- 
-void aesTest (int bits)
-{
-  aes.iv_inc();
-  
-  byte iv [N_BLOCK] ;
-  int plainPaddedLength = sizeof(plain) + (N_BLOCK - ((sizeof(plain)-1) % 16)); // length of padded plaintext [B]
-  byte cipher [plainPaddedLength]; // ciphertext (encrypted plaintext)
-  byte check [plainPaddedLength]; // decrypted plaintext
-  
-  aes.set_IV(myIv);
-  aes.get_IV(iv);
- 
-  Serial.print("- encryption time [us]: ");
-  unsigned long ms = micros ();
-  aes.do_aes_encrypt(plain,sizeof(plain),cipher,key,bits,iv);
-  Serial.println(micros() - ms);
- 
-  aes.set_IV(myIv);
-  aes.get_IV(iv);
-  
-  Serial.print("- decryption time [us]: ");
-  ms = micros ();
-  aes.do_aes_decrypt(cipher,aes.get_size(),check,key,bits,iv); 
-  Serial.println(micros() - ms);
-  
-  Serial.print("- plain:   ");
-  aes.printArray(plain,(bool)true); //Message envoyé
+}
 
-  Serial.print("- plainPaddedLength:   ");
-  Serial.println(plainPaddedLength);
-  
-  Serial.print("- cipher:  ");
-  aes.printArray(cipher,(bool)false); //Message crypté
+void setup() {
  
-  Serial.print("- check:   ");
-  aes.printArray(check,(bool)true); //Message dechiffré
+   /* Initialisation du port série */
+  Serial.begin(115200);
+  delayMicroseconds(10);
+  /* Initialisation radio */
+  radio.begin();
+  radio.setPALevel(RF24_PA_MIN);
+  radio.setChannel(100);
+  radio.setDataRate(RF24_1MBPS);
+  radio.setAutoAck(0);
+  radio.enableDynamicPayloads();
+  radio.openWritingPipe(addresses[0]);                              // 03499 
+  radio.openReadingPipe(1, addresses[1]);                           // 02989                            
+  radio.printDetails();
+  delayMicroseconds(10);
+}
 
-  Serial.print("- iv:      ");
-  aes.printArray(iv,16); //print iv
-   
-  printf("\n===================================================================================\n");
+void loop() {
+aes();  
+
+    /*  Serial.println("Reception de la confirmation");
+      radio.startListening(); 
+      while (radio.available()){ 
+        radio.read(&message, sizeof(message)); 
+        */
 }
